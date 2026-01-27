75c3edc2880ce097e7f1e195115f3940fb96cdf0
siqjulia <jusiqueira@davidson.edu>
2026-01-27 17:45:24 -0500

    Linked custom.css in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,9 +10,14 @@ export default defineConfig({
 	integrations: [
 		// Startlight-specific config
 		starlight({
-			title: 'Owen\'s DevLog',
+			title: 'Julia\'s DevLog',
+
 			sidebar: [
 				{ label: 'Schedule', autogenerate: { directory: 'schedule' }, },
+		
+			],
+			customCss: [
+				'./src/styles/custom.css', // relative path to custom CSS file
 			],
 		}),
 	],
